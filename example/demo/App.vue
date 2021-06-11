17af02d262ca8a0f4916b8b8c2dc12df1b826b7a
Kdump <rootshellexp@gmail.com>
2021-06-11 17:43:24 +0800

    实验自动添加元素的id
    实验自动添加元素的id

--- example/demo/App.vue
+++ example/demo/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="app">
-    <p data-test="abc" data-xxx="123">
-      This sinmple page is meant to demo the plugin's behaviour. Depending on which <code>serve</code> 
-      command you used, the paragrpah that you are reading has a <code>data-test</code> attribute - or it doesn't
+    <p data-test="abc" xxx="233sd3" xxx2="2222" data-xxx="123">
+      This sinmple page is meant to demo the plugin's behaviour. Depending on
+      which <code>serve</code> command you used, the paragrpah that you are
+      reading has a <code>data-test</code> attribute - or it doesn't
+    </p>
+    <p data-test="abc" xxx="233sd3" xxx2="2222" data-xxx="123">
+      This sinmple page is meant to demo the plugin's behaviour. Depending on
+      which <code>serve</code> command you used, the paragrpah that you are
+      reading has a <code>data-test</code> attribute - or it doesn't
     </p>
   </div>
 </template>
